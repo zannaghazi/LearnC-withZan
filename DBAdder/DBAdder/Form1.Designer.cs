1f2e63ee9b2be3916de54f327e8ecba6a1260509
weko3-dev22 <weko3-hitachi-dev22@ml.itg.hitachi.co.jp>
2019-05-09 17:28:36 +0700

    Add new dialog, add UI function

--- DBAdder/DBAdder/Form1.Designer.cs
+++ DBAdder/DBAdder/Form1.Designer.cs
@@ -30,20 +30,22 @@
         {
             this.dataTable = new System.Windows.Forms.DataGridView();
             this.tableControl = new System.Windows.Forms.Panel();
-            this.txtMode = new System.Windows.Forms.TextBox();
-            this.btnConnect = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtDBName = new System.Windows.Forms.TextBox();
-            this.txtTableName = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.cbDBType = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.btnAddTable = new System.Windows.Forms.Button();
-            this.btnEditTable = new System.Windows.Forms.Button();
-            this.btnAddCol = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAddCol = new System.Windows.Forms.Button();
+            this.btnEditTable = new System.Windows.Forms.Button();
+            this.btnAddTable = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbDBType = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTableName = new System.Windows.Forms.TextBox();
+            this.txtDBName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtMode = new System.Windows.Forms.TextBox();
+            this.pnData = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtShowTable = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataTable)).BeginInit();
             this.tableControl.SuspendLayout();
             this.SuspendLayout();
@@ -51,9 +53,9 @@
             // dataTable
             // 
             this.dataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataTable.Location = new System.Drawing.Point(12, 12);
+            this.dataTable.Location = new System.Drawing.Point(12, 44);
             this.dataTable.Name = "dataTable";
-            this.dataTable.Size = new System.Drawing.Size(467, 537);
+            this.dataTable.Size = new System.Drawing.Size(467, 505);
             this.dataTable.TabIndex = 0;
             // 
             // tableControl
@@ -77,68 +79,61 @@
             this.tableControl.Size = new System.Drawing.Size(302, 182);
             this.tableControl.TabIndex = 1;
             // 
-            // txtMode
+            // btnReset
             // 
-            this.txtMode.Enabled = false;
-            this.txtMode.Location = new System.Drawing.Point(71, 4);
-            this.txtMode.Name = "txtMode";
-            this.txtMode.Size = new System.Drawing.Size(134, 20);
-            this.txtMode.TabIndex = 0;
-            this.txtMode.Text = "Script";
+            this.btnReset.Enabled = false;
+            this.btnReset.Location = new System.Drawing.Point(154, 149);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(137, 23);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset Table";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
-            // btnConnect
+            // btnAddCol
             // 
-            this.btnConnect.Location = new System.Drawing.Point(212, 3);
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(85, 23);
-            this.btnConnect.TabIndex = 1;
-            this.btnConnect.Text = "Connect...";
-            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnAddCol.Enabled = false;
+            this.btnAddCol.Location = new System.Drawing.Point(11, 149);
+            this.btnAddCol.Name = "btnAddCol";
+            this.btnAddCol.Size = new System.Drawing.Size(137, 23);
+            this.btnAddCol.TabIndex = 12;
+            this.btnAddCol.Text = "Add Column";
+            this.btnAddCol.UseVisualStyleBackColor = true;
+            this.btnAddCol.Click += new System.EventHandler(this.btnAddCol_Click);
             // 
-            // label1
+            // btnEditTable
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 8);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Mode";
+            this.btnEditTable.Enabled = false;
+            this.btnEditTable.Location = new System.Drawing.Point(154, 120);
+            this.btnEditTable.Name = "btnEditTable";
+            this.btnEditTable.Size = new System.Drawing.Size(137, 23);
+            this.btnEditTable.TabIndex = 11;
+            this.btnEditTable.Text = "Edit Table";
+            this.btnEditTable.UseVisualStyleBackColor = true;
+            this.btnEditTable.Click += new System.EventHandler(this.btnEditTable_Click);
             // 
-            // txtDBName
+            // btnAddTable
             // 
-            this.txtDBName.Enabled = false;
-            this.txtDBName.Location = new System.Drawing.Point(71, 31);
-            this.txtDBName.Name = "txtDBName";
-            this.txtDBName.Size = new System.Drawing.Size(226, 20);
-            this.txtDBName.TabIndex = 3;
+            this.btnAddTable.Location = new System.Drawing.Point(11, 120);
+            this.btnAddTable.Name = "btnAddTable";
+            this.btnAddTable.Size = new System.Drawing.Size(137, 23);
+            this.btnAddTable.TabIndex = 10;
+            this.btnAddTable.Text = "Add Table";
+            this.btnAddTable.UseVisualStyleBackColor = true;
+            this.btnAddTable.Click += new System.EventHandler(this.btnAddTable_Click);
             // 
-            // txtTableName
+            // label4
             // 
-            this.txtTableName.Location = new System.Drawing.Point(71, 85);
-            this.txtTableName.Name = "txtTableName";
-            this.txtTableName.Size = new System.Drawing.Size(226, 20);
-            this.txtTableName.TabIndex = 5;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 33);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "DB name";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 88);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Table name";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "DB Type";
             // 
             // cbDBType
             // 
+            this.cbDBType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbDBType.FormattingEnabled = true;
             this.cbDBType.Items.AddRange(new object[] {
             "Microsoft SQL Server (1998 - 2012)",
@@ -151,80 +146,115 @@
             this.cbDBType.Name = "cbDBType";
             this.cbDBType.Size = new System.Drawing.Size(226, 21);
             this.cbDBType.TabIndex = 8;
-            this.cbDBType.Text = "Select the DB Type";
             // 
-            // label4
+            // label3
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 60);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "DB Type";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Table name";
             // 
-            // btnAddTable
+            // label2
             // 
-            this.btnAddTable.Location = new System.Drawing.Point(11, 120);
-            this.btnAddTable.Name = "btnAddTable";
-            this.btnAddTable.Size = new System.Drawing.Size(137, 23);
-            this.btnAddTable.TabIndex = 10;
-            this.btnAddTable.Text = "Add Table";
-            this.btnAddTable.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "DB name";
             // 
-            // btnEditTable
+            // txtTableName
             // 
-            this.btnEditTable.Enabled = false;
-            this.btnEditTable.Location = new System.Drawing.Point(154, 120);
-            this.btnEditTable.Name = "btnEditTable";
-            this.btnEditTable.Size = new System.Drawing.Size(137, 23);
-            this.btnEditTable.TabIndex = 11;
-            this.btnEditTable.Text = "Edit Table";
-            this.btnEditTable.UseVisualStyleBackColor = true;
+            this.txtTableName.Location = new System.Drawing.Point(71, 85);
+            this.txtTableName.Name = "txtTableName";
+            this.txtTableName.Size = new System.Drawing.Size(226, 20);
+            this.txtTableName.TabIndex = 5;
             // 
-            // btnAddCol
+            // txtDBName
             // 
-            this.btnAddCol.Enabled = false;
-            this.btnAddCol.Location = new System.Drawing.Point(11, 149);
-            this.btnAddCol.Name = "btnAddCol";
-            this.btnAddCol.Size = new System.Drawing.Size(137, 23);
-            this.btnAddCol.TabIndex = 12;
-            this.btnAddCol.Text = "Add Column";
-            this.btnAddCol.UseVisualStyleBackColor = true;
+            this.txtDBName.Enabled = false;
+            this.txtDBName.Location = new System.Drawing.Point(71, 31);
+            this.txtDBName.Name = "txtDBName";
+            this.txtDBName.Size = new System.Drawing.Size(226, 20);
+            this.txtDBName.TabIndex = 3;
             // 
-            // btnReset
+            // label1
             // 
-            this.btnReset.Enabled = false;
-            this.btnReset.Location = new System.Drawing.Point(154, 149);
-            this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(137, 23);
-            this.btnReset.TabIndex = 13;
-            this.btnReset.Text = "Reset Table";
-            this.btnReset.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mode";
             // 
-            // panel1
+            // btnConnect
             // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Location = new System.Drawing.Point(486, 201);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(302, 255);
-            this.panel1.TabIndex = 2;
+            this.btnConnect.Location = new System.Drawing.Point(212, 3);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(85, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect...";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtMode
+            // 
+            this.txtMode.Enabled = false;
+            this.txtMode.Location = new System.Drawing.Point(71, 4);
+            this.txtMode.Name = "txtMode";
+            this.txtMode.Size = new System.Drawing.Size(134, 20);
+            this.txtMode.TabIndex = 0;
+            this.txtMode.Text = "Script";
+            // 
+            // pnData
+            // 
+            this.pnData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnData.Location = new System.Drawing.Point(486, 201);
+            this.pnData.Name = "pnData";
+            this.pnData.Size = new System.Drawing.Size(302, 348);
+            this.pnData.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Table Name: ";
+            // 
+            // txtShowTable
+            // 
+            this.txtShowTable.AutoSize = true;
+            this.txtShowTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtShowTable.Location = new System.Drawing.Point(132, 16);
+            this.txtShowTable.Name = "txtShowTable";
+            this.txtShowTable.Size = new System.Drawing.Size(0, 20);
+            this.txtShowTable.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 561);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtShowTable);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pnData);
             this.Controls.Add(this.tableControl);
             this.Controls.Add(this.dataTable);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataTable)).EndInit();
             this.tableControl.ResumeLayout(false);
             this.tableControl.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -245,7 +275,9 @@
         private System.Windows.Forms.Button btnAddCol;
         private System.Windows.Forms.Button btnEditTable;
         private System.Windows.Forms.Button btnAddTable;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnData;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label txtShowTable;
     }
 }
 
--- DBAdder/DBAdder/Form1.cs
+++ DBAdder/DBAdder/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DBAdder.Dialog;
 
 namespace DBAdder
 {
@@ -15,6 +16,142 @@ namespace DBAdder
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private int colIndex = 0;
+        private string tableName = "";
+        private string sessionTableName = "";
+        private int sesstionDBType = -1;
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            DBConnection dbInfoDialog = new DBConnection();
+            dbInfoDialog.ShowDialog();
+            DBInfo dbInfo = dbInfoDialog.dataInfo;
+            if (dbInfoDialog.isEdited)
+            { 
+                this.txtDBName.Text = dbInfo.DBName;
+                this.txtMode.Text = "DB Connection";
+                if (dbInfo.dataType == "")
+                {
+                    this.cbDBType.SelectedItem = null;
+                    this.cbDBType.Enabled = true;
+                }
+                else
+                {
+                    if (dbInfo.dataType == "Microsoft SQL Server")
+                    {
+                        this.cbDBType.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        this.cbDBType.SelectedIndex = 3; // PostgreSQL
+                    }
+                    this.cbDBType.Enabled = false;
+                }
+            }
+        }
+
+        private void btnAddTable_Click(object sender, EventArgs e)
+        {
+            if (this.cbDBType.SelectedItem == null)
+            {
+                MessageBox.Show("Database type is required!");
+                return;
+            }
+            if (String.IsNullOrWhiteSpace(this.txtTableName.Text))
+            {
+                MessageBox.Show("Table name is required!");
+                return;
+            }
+            this.txtShowTable.Text = this.txtTableName.Text;
+            this.tableName = this.txtTableName.Text;
+            //this.dataTable.Columns.Add("col" + this.colIndex, tableName);
+            //this.colIndex++;
+
+            //Disable all table info
+            this.btnConnect.Enabled = false;
+            this.txtTableName.Enabled = false;
+            this.btnAddTable.Enabled = false;
+            this.cbDBType.Enabled = false;
+
+            // Enable all table content
+            this.btnEditTable.Enabled = true;
+            this.btnAddCol.Enabled = true;
+            this.btnReset.Enabled = true;
+
+            if (this.btnAddTable.Text == "OK")
+            {
+                this.btnAddTable.Text = "Add Table";
+                this.btnEditTable.Text = "Edit Table";
+            }
+        }
+
+        private void btnEditTable_Click(object sender, EventArgs e)
+        {
+            if (this.btnEditTable.Text == "Edit Table")
+            {
+                this.sessionTableName = this.txtTableName.Text;
+                this.sesstionDBType = this.cbDBType.SelectedIndex;
+                // Enable all table info
+                this.txtTableName.Enabled = true;
+                this.btnAddTable.Enabled = true;
+                this.btnAddTable.Text = "OK";
+                if (String.IsNullOrWhiteSpace(this.txtDBName.Text))
+                {
+                    this.cbDBType.Enabled = true;
+                }
+                
+                // Disable all table content
+                this.btnEditTable.Enabled = true;
+                this.btnAddCol.Enabled = false;
+                this.btnReset.Enabled = false;
+
+                this.btnEditTable.Text = "Cancel";
+            }
+            else
+            {
+                this.txtTableName.Text = this.sessionTableName;
+                this.cbDBType.SelectedIndex = this.sesstionDBType;
+                //Disable all table info
+                this.btnConnect.Enabled = false;
+                this.txtTableName.Enabled = false;
+                this.btnAddTable.Enabled = false;
+                this.cbDBType.Enabled = false;
+
+                // Enable all table content
+                this.btnEditTable.Enabled = true;
+                this.btnAddCol.Enabled = true;
+                this.btnReset.Enabled = true;
+
+                this.btnAddTable.Text = "Add Table";
+                this.btnEditTable.Text = "Edit Table";
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            this.txtShowTable.Text = "";
+            this.dataTable = new DataGridView();
+            this.txtMode.Text = "Script";
+            this.btnConnect.Enabled = true;
+            this.txtDBName.Text = "";
+            this.cbDBType.SelectedItem = "";
+            this.txtTableName.Text = "";
+            this.btnAddTable.Enabled = true;
+            this.btnEditTable.Enabled = false;
+            this.btnAddCol.Enabled = false;
+            this.btnReset.Enabled = false;
+
+            this.colIndex = 0;
+            this.tableName = "";
+            this.sessionTableName = "";
+            this.sesstionDBType = -1;
+    }
+
+        private void btnAddCol_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
